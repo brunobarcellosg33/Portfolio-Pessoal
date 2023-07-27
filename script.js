bbf1d619959dc82224a76e46cb0dfc3a2e2a7ba4
Bruno Giovanni Barcellos <107158560+brunobarcellosg33@users.noreply.github.com>
2023-07-27 20:32:01 -0300

    Update script.js
    Upd JS tabs separadas.

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
-
-function showSlide(slideIndex) {
-  slides.forEach((slide, index) => {
-    if (index === slideIndex) {
-      slide.classList.add('active');
-    } else {
-      slide.classList.remove('active');
-    }
-  });
-}
-
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}
-
-function prevSlide() {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(currentSlide);
-}
-
-const prevButton = document.querySelector('.prev-btn');
-const nextButton = document.querySelector('.next-btn');
-
-prevButton.addEventListener('click', prevSlide);
-nextButton.addEventListener('click', nextSlide);
-
-showSlide(currentSlide);
-
 function openTab(event, tabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
