c233fe55276d352b9c4851ce1492c74afce951ec
brunobarcvellosg33 <contatobrunobarcellos@hotmail.com>
2023-10-31 19:19:17 -0300

    Portfolio-Pessoal

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+function openTab(event, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("btn");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.className += " active";
+  
+};
+
+
+// Div redirectToURL clicável
+function redirectToURL(url) {
+  window.open(url, '_blank');
+}
+
+
